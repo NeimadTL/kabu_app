539fc02db0efeea7c6f7c41281781c501158af54
neimad971 <damientalbot26@gmail.com>
2018-01-07 22:06:52 +0100

    show test has been added for Service controller + bootstrap integration

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe ServicesController, type: :controller do
     end
   end
 
+
+  let(:agriculture) { Category.create!(name: 'AGRICULTURE') }
+  let(:service) {
+    Service.create!(title: 'Prestation d\'agriculture', 
+    description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essentially unchanged. It was popularised in the 1960s with the release of 
+    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+    software like Aldus PageMaker including versions of Lorem Ipsum.',
+    price: '9.9', category_id: agriculture.id)
+  }
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, id: service.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
