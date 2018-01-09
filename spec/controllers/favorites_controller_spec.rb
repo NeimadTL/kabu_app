60d53a50386f9f40bf55c6affef0f6e507a9bf60
neimad971 <damientalbot26@gmail.com>
2018-01-10 00:41:06 +0100

    tests have been added on favorites controller

---
+++ spec/controllers/favorites_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe FavoritesController, type: :controller do
+
+  let(:user) { 
+    User.create!(email: "something@gmail.com", password: "12345678", password_confirmation: "12345678")
+  }
+  let(:agriculture) { Category.create!(name: 'AGRICULTURE') }
+  let(:service) {
+    Service.create!(title: 'Prestation d\'agriculture', 
+    description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essentially unchanged. It was popularised in the 1960s with the release of 
+    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+    software like Aldus PageMaker including versions of Lorem Ipsum.',
+    price: '9.9', category_id: agriculture.id)
+  }
+
+
+  describe "when POST create" do
+    subject { user }
+
+    before do
+      sign_in(user, nil)
+      post :create, service_id: service.id
+    end
+
+    it "returns http redirect and redirects to root path" do
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(root_path)
+    end
+
+
+    describe "and when DELETE destroy" do
+      before { delete :destroy, id: service.id }
+
+      it "returns http redirect and redirects to root path" do
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+end
