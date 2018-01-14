0a5a622941a3a8b77f05df5e904a1fdf584ae0fc
neimad971 <damientalbot26@gmail.com>
2018-01-14 10:42:44 +0100

    comments controller has been reinforced so that only customer can access it. Tests have been updated accordingly too

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 
 
   before_action :authenticate_user!
+  before_action :require_to_be_customer
 
 
   def create
@@ -9,9 +10,9 @@ class CommentsController < ApplicationController
     @comment = @service.comments.create(comment_params.merge(user: current_user))
 
     if @comment.valid?
-      flash[:success] = "Commentaire ajouté"
+      flash[:notice] = "Commentaire ajouté"
     else
-      flash[:alert] = "Erreur survenue"
+      flash[:alert] = @comment.errors.full_messages
     end
 
     redirect_to service_path(@service)
@@ -22,6 +23,12 @@ class CommentsController < ApplicationController
 
     def comment_params
       params.require(:comment).permit(:score, :content)
+    end
+
+    def require_to_be_customer
+      if current_user && current_user.for_business
+        render text: "Unauthorized", status: :unauthorized
+      end
     end
 
 
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe CommentsController, type: :controller do
 
   let(:user) { 
-    User.create!(email: "something@gmail.com", password: "12345678", password_confirmation: "12345678")
+    User.create!(email: "something@gmail.com", password: "12345678", 
+      password_confirmation: "12345678", for_business: false)
   }
   let(:agriculture) { Category.create!(name: 'AGRICULTURE') }
   let(:service) {
@@ -25,22 +26,38 @@ RSpec.describe CommentsController, type: :controller do
       post :create, service_id: service.id, comment: { score: '4', content: 'awesome' } 
     end
 
-    it "with good params" do
-      expect(flash[:success]).to match(/Commentaire ajouté*/)
+    it "with authorised logged in user and good params, returns redirect" do
+      expect(flash[:notice]).to match('Commentaire ajouté')
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(service_path(service))
+    end      
+
+    it "with authorised logged in user and bad params, returns redirect and shows error messasge" do
+      post :create, service_id: service.id, comment: { score: nil, content: nil }
+      expect(flash[:alert][0]).to match("Score can't be blank")
+      expect(flash[:alert][1]).to match("Score is not a number")
+      expect(flash[:alert][2]).to match("Score is not included in the list")
+      expect(flash[:alert][3]).to match("Content can't be blank")
       expect(response).to have_http_status(:redirect)
       expect(response).to redirect_to(service_path(service))
     end
 
-    describe "and POST create" do
-
-      before { post :create, service_id: service.id, comment: { score: nil, content: nil } }
-
-      it "with bad params" do
-        expect(flash[:alert]).to match(/Erreur survenue*/)
-        expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(service_path(service))
-      end
+    it "with unauthorized logged in user and good params, returns http unauthorized" do
+      sign_out(user)
+      User.find(user.id).update_attributes(for_business: true)
+      sign_in(user, nil)
+      post :create, service_id: service.id, comment: { score: '4', content: 'awesome' } 
+      expect(response).to have_http_status(:unauthorized)
+      expect(response.body).to eq "Unauthorized"
     end
+
+    it "with no user logged in,returns http redirect" do
+      sign_out(user)
+      post :create, service_id: service.id, comment: { score: '4', content: 'awesome' } 
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+    
   end
 
 end
