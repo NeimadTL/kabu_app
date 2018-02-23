b86a45e6c16a8b6819982dba7522f211b8f126e4
neimad971 <damientalbot26@gmail.com>
2018-02-23 18:25:24 +0100

    Users can sign up with more details now (firstname, lastname, address, phonenumber)

--- app/controllers/customised_registrations_controller.rb
+++ app/controllers/customised_registrations_controller.rb
@@ -3,10 +3,11 @@ class CustomisedRegistrationsController < Devise::RegistrationsController
   def create
     super
   end
- 
+
   private
- 
+
     def sign_up_params
-      params.require(resource_name).permit(:email, :password, :password_confirmation, :for_business)
+      params.require(resource_name).permit(:firstname, :lastname, :address, :phonenumber,
+        :email, :password, :password_confirmation, :for_business)
     end
 end
